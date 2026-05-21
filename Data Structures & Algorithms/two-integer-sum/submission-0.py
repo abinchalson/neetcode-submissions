2207a6ac7b0d25aba0750a98a77ef18d3d151b1e
abinchalson <52565881+abinchalson@users.noreply.github.com>
2026-05-21 11:19:54 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index_map = {}
+        for index, num in enumerate(nums):
+            index_map[num] = index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in index_map and index_map[complement] != i:
+                indices = [i, index_map[complement]]
+                indices.sort()
+                return indices
+        return -1
